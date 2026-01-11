77300007fe775c9e0ad0211073c01826e2b07c7d
n-ikor <jaymesikr@outlook.com>
2026-01-11 08:02:22 +0100

    Week 01 submission

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please enter your grade percentage: ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        if (percent >= 80)
+        {
+            letter = "B";
+        }
+        if (percent >= 70)
+        {
+            letter = "C";
+        }
+        if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulation, You passed");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Part 1 where the user provides the magic number
+
+        // Console.Write("What is your magic number? ");
+        // string questionNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(questionNumber);
+
+        // Console.Write("What is your guess? ");
+        // string questionGuess = Console.ReadLine();
+        // int guess = int.Parse(questionGuess);
+
+        // while (guess != magicNumber)
+        // {
+        //     Console.Write("What is your guess? ");
+        //     guess = int.Parse(Console.ReadLine());
+
+        //     if (magicNumber > guess)
+        //     {
+        //         Console.WriteLine("Higher");
+        //     }
+        //     else if (magicNumber < guess)
+        //     {
+        //         Console.WriteLine("Lower");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("You guessed it right!");
+        //     }
+
+        // PART 3: USING  RANDOM TO GENERATE MAGIC NUMBER
+        // INCLUDING STRETCH CHALLENGE 
+
+        string playResponse = "yes";
+
+        while (playResponse == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulation, you guessed it right!");
+                    Console.WriteLine($"You made {guessCount} guesses.");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playResponse = Console.ReadLine().ToLower();
+        }
     }
 }
